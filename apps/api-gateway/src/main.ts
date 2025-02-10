d617c358e6f6647570ba9b93bcb54d65ce0b8f66
alireza <alireza@loyyo.nl>
2025-02-10 17:35:16 +0100

    connect api gateway to rabbitMq

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RMQ_URL ?? 'amqp://localhost:5672'],
+      queue: process.env.RMQ_QUEUE,
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.port ?? 3000);
 }
+
 bootstrap();
