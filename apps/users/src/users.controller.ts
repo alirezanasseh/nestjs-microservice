ee179adcb70ee49bf857081122ee05dbd2294a3d
alireza <alireza@loyyo.nl>
2025-02-10 17:32:33 +0100

    add a test register to user service

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  register() {
+    return this.usersService.register();
+  }
 
   @Get()
   getHello(): string {
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -5,4 +5,8 @@ export class UsersService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  register() {
+    return 'Register';
+  }
 }
