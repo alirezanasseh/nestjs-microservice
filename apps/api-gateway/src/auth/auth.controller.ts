b6b2125604afbb602ce9606cefbcf3e62a443503
alireza <alireza@loyyo.nl>
2025-02-11 12:31:23 +0100

    move register and login logic to service

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -1,49 +1,25 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
 import { RegisterDto } from './dto/register.dto';
-import { AuthReturnDto } from './dto/auth-return.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { UserContext } from './decorators/user.decorator';
 import { UserType } from '../types/user.type';
-import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   usersUrl: string;
 
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly configService: ConfigService,
-  ) {
-    this.usersUrl =
-      this.configService.get<string>('USERS_SERVICE_URL') ||
-      'http://users:3000';
-    if (this.usersUrl === '') {
-      throw new Error('Could not find users service');
-    }
-  }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto) {
-    return this.httpService.axiosRef
-      .post<AuthReturnDto>(this.usersUrl + '/register', registerDto)
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.error(error);
-        throw new Error('Could not register user');
-      });
+  async register(@Body() data: RegisterDto) {
+    return this.authService.register(data);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    const response = await this.httpService.axiosRef.post<AuthReturnDto>(
-      this.usersUrl + '/auth/login',
-      loginDto,
-    );
-    return response.data;
+  async login(@Body() data: LoginDto) {
+    return this.authService.login(data);
   }
 
   @UseGuards(JwtAuthGuard)
--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -3,10 +3,28 @@ import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
 import { UserType } from '../types/user.type';
 import { JwtPayloadType } from '../types/jwt-payload.type';
+import { HttpService } from '@nestjs/axios';
+import { RegisterDto } from './dto/register.dto';
+import { AuthReturnDto } from './dto/auth-return.dto';
+import { ConfigService } from '@nestjs/config';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  usersUrl: string;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.usersUrl =
+      this.configService.get<string>('USERS_SERVICE_URL') ||
+      'http://users:3000';
+    if (this.usersUrl === '') {
+      throw new Error('Could not find users service');
+    }
+  }
 
   async hashPassword(password: string): Promise<string> {
     return await argon2.hash(password);
@@ -22,5 +40,25 @@ export class AuthService {
   generateToken(user: UserType) {
     const payload: JwtPayloadType = { sub: user.id, user };
     return this.jwtService.sign(payload);
+  }
+
+  async register(data: RegisterDto) {
+    return this.httpService.axiosRef
+      .post<AuthReturnDto>(this.usersUrl + '/register', data)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+        throw new Error('Could not register user');
+      });
+  }
+
+  async login(data: LoginDto) {
+    const response = await this.httpService.axiosRef.post<AuthReturnDto>(
+      this.usersUrl + '/login',
+      data,
+    );
+    return response.data;
   }
 }
