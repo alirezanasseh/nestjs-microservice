f363fbb231ecf6e6e703e0ab2e329d7daef12f34
Alireza <alireza.nasseh@gmail.com>
2025-02-26 23:58:25 +0100

    make port uppercase in main

--- apps/cqrs/src/main.ts
+++ apps/cqrs/src/main.ts
@@ -3,6 +3,6 @@ import { CqrsModule } from './cqrs.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CqrsModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
